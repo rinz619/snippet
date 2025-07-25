2b5c1b92557fb7edd20d681dc9775b35aaf6dffe
unknown <rinshad619@gmail.com>
2025-07-26 02:08:20 +0530

    initial

--- api/urls.py
+++ api/urls.py
@@ -5,11 +5,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = 'api'
 urlpatterns = [
-    path('login',views.login.as_view(),name="login"),
-    
-    path('tagslist',views.tagslist.as_view(),name="tagslist"),
-    path('snippetlist',views.snippetlist.as_view(),name="snippetlist"),
-    path('snippetdetails',views.snippetdetails.as_view(),name="snippetdetails"),
-    path('snippetdelete',views.snippetdelete.as_view(),name="snippetdelete"),
+   
     
 ]
